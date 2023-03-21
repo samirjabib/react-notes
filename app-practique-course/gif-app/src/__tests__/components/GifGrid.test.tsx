b1a53e03645f3fef03bc21ecdcab2a598a2e9675
samirdev9669 <samirdev9669@gmail.com>
2023-03-21 16:21:35 -0500

    add

--- app-practique-course/gif-app/src/__tests__/components/GifGrid.test.tsx
+++ app-practique-course/gif-app/src/__tests__/components/GifGrid.test.tsx
@@ -1,12 +1,47 @@
-import { render, screen } from '@testing-library/react'
-import { GifGrid } from '../../components/GifGrid';
-import { useFetchGifs } from '../../hooks/useFetchGifs';
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
 
-jest.mock('../../hooks/useFetcbGifs') //Mock the custom hook
+import React from "react";
 
+jest.mock("../../hooks/useFetchGifs"); //Mock the custom hook
 
-describe('Test in <GifGrid/>', () => {
-    
-    test('should be show loading in the init', () => {
+const mockedFetchGifs = jest.mocked(useFetchGifs); // use this for mocked the hook on react.
+
+describe.only("Test in <GifGrid/>", () => {
+  const category = "One Punch";
+
+  test("should be show loading in the init", () => {
+    mockedFetchGifs.mockReturnValue({
+      gifs: [],
+      isLoading: true,
+    });
+
+    render(<GifGrid category={category} />);
+    expect(screen.getByText('Cargando...'))
+    expect(screen.getByText(category))
+  });
+
+  test('should show items when this is charge', () =>{
+    const images = [
+        {
+            id: 'ABC',
+            title: 'Saitama',
+            url: 'https://localhost/saitama.jpg'
+        },
+        {
+            id: '123',
+            title: 'Goku',
+            url: 'https://localhost/goku.jpg'
+        },
+    ]
+
+    mockedFetchGifs.mockReturnValue({
+        gifs:images,
+        isLoading:false,
     })
-})
+  })
+
+  render(<GifGrid category={category}/>)
+  expect(screen.getAllByRole('img').length).toBe(2)
+});
--- app-practique-course/gif-app/src/hooks/useFetchGifs.ts
+++ app-practique-course/gif-app/src/hooks/useFetchGifs.ts
@@ -1,7 +1,17 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
+
+type Gif = {
+  id:string,
+  title:string,
+  url:string
+}
+
 export const useFetchGifs = (category: string) => {
-  const [gifs, setGifs] = useState<[]>([]);
+
+
+
+  const [gifs, setGifs] = useState<Gif[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const getImages = async () => {
