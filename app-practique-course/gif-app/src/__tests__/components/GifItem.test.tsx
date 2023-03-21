7a7eb19dc7c096adbf72a2f79f28ec882b58cd77
samirdev9669 <samirdev9669@gmail.com>
2023-03-20 20:47:59 -0500

    add

--- app-practique-course/gif-app/src/__tests__/components/GifItem.test.tsx
+++ app-practique-course/gif-app/src/__tests__/components/GifItem.test.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 
 import { GifItem } from "../../components/GifItem";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("test in <GifItem/>", () => {
   const title = "saitama";
   const url = "htttps://one-puch.com/saitama.jpg";
 
   test("gif item should be render props", () => {
+    const { container } = render(<GifItem title={title} url={url} />); //Extract container for compare this with the snapshot
+    expect(container).toMatchSnapshot();
+  });
+
+  test("gif should be imagen with the exact url and alt", () => {
     render(<GifItem title={title} url={url} />);
+    // screen.debug() //this is a console.log for component. 
+    
+    const {  } = screen.getByRole('img')
+    c
+    
+
+
   });
 });
